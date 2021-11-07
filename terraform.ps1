74d953fea0f6b5e6d14b0851b48ee59928cea75d
Jacky Fan <69104346+jfan888@users.noreply.github.com>
2021-11-07 22:07:33 +1300

    add install chocolatey and chrome package

--- terraform.ps1
+++ terraform.ps1
@@ -1,4 +1,10 @@
 Set-TimeZone -Name "New Zealand Standard Time"
 
+Set-ExecutionPolicy Bypass -Scope Process -Force; [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072; iex ((New-Object System.Net.WebClient).DownloadString('https://community.chocolatey.org/install.ps1'))
+
+Start-sleep 10
+
+choco install googlechrome
+
 Install-WindowsFeature -Name telnet-client
 Install-WindowsFeature -Name Hyper-V -IncludeManagementTools -Restart
